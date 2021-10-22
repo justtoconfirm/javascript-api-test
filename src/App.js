ca676ca032c6e46354f80d307ebab0196a7f871e
justtoconfirm <office@justtoconfirm.co.uk>
2021-10-22 16:40:06 +0100

    FetchAPI and hook used to return the beers from the API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from './components/Layout';
 
 const App = () => {
+
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch("https://api.punkapi.com/v2/beers")
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setItems(result);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            );
+    }, []);
+
     return (
 
         <Layout>
@@ -9,7 +29,15 @@ const App = () => {
             <div className="container">
                 <div className="row">
 
-                    <h1>Hello World</h1>
+                    <section>
+                    
+                        <h1>Hello World</h1>
+
+                        {items.map((item) => (
+                            console.log(item)
+                        ))}
+
+                    </section>
 
                 </div>
             </div>
